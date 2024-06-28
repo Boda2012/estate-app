d377415821a712c26cf031050f929dd39e08c58a
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-06-28 19:06:35 +0200

    verifyToken middleware

--- api/routes/test.route.js
+++ api/routes/test.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-// import {verifyToken} from "../middleware/verifyToken.js";
+import {verifyToken} from "../middleware/verifyToken.js";
 import { shouldBeAdmin, shouldBeLoggedIn } from "../controllers/test.controller.js";
 
 const router = express.Router();
 
-router.get("/should-be-logged-in",  shouldBeLoggedIn);
-// verifyToken,
+router.get("/should-be-logged-in",verifyToken,  shouldBeLoggedIn);
+
 router.get("/should-be-admin", shouldBeAdmin);
 
 export default router;
