f687bfe6211738e9f03f2a2c2892cb5c7dcf32ed
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-06-28 18:36:17 +0200

    verify token

--- api/app.js
+++ api/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
-// import testRoute from "./routes/test.route.js";
+import testRoute from "./routes/test.route.js";
 // import userRoute from "./routes/user.route.js";
 // import chatRoute from "./routes/chat.route.js";
 // import messageRoute from "./routes/message.route.js";
@@ -16,13 +16,13 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/test", (req, res) => {
-  res.send("It is a live!!!")
-})
+// app.use("/api/test", (req, res) => {
+//   res.send("It is a live!!!")
+// })
 app.use("/api/auth", authRoute);
 // app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
-// app.use("/api/test", testRoute);
+app.use("/api/test", testRoute);
 // app.use("/api/chats", chatRoute);
 // app.use("/api/messages", messageRoute);
 
