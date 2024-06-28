27b1e27f2e24e1cbbef0eba8f5fc0278ad3b391b
Bogoljub Simonovic <bogoljub2012@gmail.com>
2024-06-28 14:34:05 +0200

    starting with frontend

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -2,10 +2,24 @@ import "./register.scss";
 import { Link } from "react-router-dom";
 
 function Register() {
+
+  const handleSubmit =  (e) => {
+
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    console.log(username, email, password);
+  }
+
   return (
     <div className="register">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
